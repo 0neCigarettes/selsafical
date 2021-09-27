62160fb632fc85e883ce9c65953d8868147f7e8f
blackcoffelate <juli.iot@ubl.ac.id>
2021-09-27 23:32:56 +0700

    origin

--- src/pages/pengeluaran/form.vue
+++ src/pages/pengeluaran/form.vue
@@ -44,7 +44,7 @@
                         <q-select
                           filled
                           v-model="product.product"
-                          label="Nama barang"
+                          label="Supplier / Vendor"
                           option-value="_id"
                           option-label="nama_product"
                           :options="options.products"
@@ -63,6 +63,31 @@
                         </q-select>
                       </div>
                       <div class="col">
+                        <q-select
+                          filled
+                          v-model="product.product"
+                          label="Nama barang"
+                          option-value="_id"
+                          option-label="nama_product"
+                          :options="options.products"
+                          use-input
+                          @filter="filterProduct"
+                          outlined
+                          dense
+                        >
+                          <template v-slot:no-option>
+                            <q-item>
+                              <q-item-section class="text-grey">
+                                Produk tidak ditemukan
+                              </q-item-section>
+                            </q-item>
+                          </template>
+                        </q-select>
+                      </div>
+                    </div>
+
+                    <div class="row q-gutter-sm">
+                      <div class="col">
                         <q-input
                           filled
                           v-model="product.jumlah_penjualan"
@@ -71,6 +96,8 @@
                           dense
                           :rules="[ val => val && val > 0 || 'Jumlah penjualan tidak boleh 0 / kosong !']"
                         />
+                      </div>
+                      <div class="col">
                       </div>
                     </div>
 
@@ -303,7 +330,11 @@ export default {
         this.form.products[i].product = null
         this.form.products[i].jumlah_penjualan = null
         if (i > 0) {
+<<<<<<< HEAD
           this.form.products.splice(i)
+=======
+          this.form.products.splice(i, 1)
+>>>>>>> 3e38128 (kumity)
         }
       }
       this.form.tanggal_jatuh_tempo = null
